04175002bbecc2a81a8714fdf405b983c2a28c34
Raymond Looney <rlooney@uvm.edu>
2020-10-08 12:38:48 -0400

    Added code on how to calculate eigenvalues using eigen() function & included management strategies.

--- Problem_set_3.R
+++ Problem_set_3.R
@@ -2,3 +2,11 @@
 # Problem set 3
 
 # This script will be used to share code
+
+#eigenvalue problems:
+
+#Filter the COMADRE dataset to incorporate just the killer whale:
+load("~/Desktop/UVM/UVM Courses/Quant Reasoning/Looney_Futia_Problem_set_3/COMADRE_v.4.20.9.0.RData")
+com = comadre$metadata
+library(dplyr)
+kw = filter(com, SpeciesAccepted == 'Orcinus orca')
